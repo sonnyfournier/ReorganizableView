172b9429aecaf166e7554e41d94590eab6fae092
Sonny Fournier <sonny.fournier@protonmail.com>
2023-06-20 14:07:46 +0200

    :fire: Remove storyboards in example app

--- Example App/ReorganizableViewExampleApp/ViewController.swift
+++ Example App/ReorganizableViewExampleApp/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: - UI elements
+    //
+
+    // MARK: - Properties
+    //
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+        setupConstraints()
+    }
+
+    // MARK: - UI setup
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+    // MARK: - Constraints setup
+    private func setupConstraints() {
+        //
+    }
+
+    // MARK: - Actions
+    //
+
+    // MARK: - Functions
+    //
 
 }
-
