fffae67bef69ac8d41d44d8ddee989541c658f54
Sonny Fournier <sonny.fournier@protonmail.com>
2023-06-20 13:54:54 +0200

    🎉 Create project

---
+++ Sources/ReorganizableView/ReorganizableView.h
@@ -0,0 +1,18 @@
+//
+//  ReorganizableView.h
+//  ReorganizableView
+//
+//  Created by Sonny Fournier on 20/06/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReorganizableView.
+FOUNDATION_EXPORT double ReorganizableViewVersionNumber;
+
+//! Project version string for ReorganizableView.
+FOUNDATION_EXPORT const unsigned char ReorganizableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReorganizableView/PublicHeader.h>
+
+
